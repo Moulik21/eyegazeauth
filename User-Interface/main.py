bbf1b18638f7ed8510bec94f0dad89adaa087b6f
Keenan Kua <keenankua@outlook.com>
2020-02-27 18:49:52 -0500

    Add select points screen for sample images

--- User-Interface/main.py
+++ User-Interface/main.py
@@ -1,8 +1,10 @@
 import os
-
 import wx
 import webbrowser
 import wx.lib.buttons as buts
+
+# get current file directory
+curdir = os.path.dirname(os.path.realpath(__file__))
 
 class MyApp(wx.App):
     def __init__(self):
@@ -16,7 +18,6 @@ class MyApp(wx.App):
         frame.SetSize(wx.Size(600, 500))
         frame.Show()
 
-
 class MainFrame(wx.Frame):
     # subclass of wx.Window; Frame is a top level window
     # A frame is a window whose size and position can (usually) be changed by the user.
@@ -27,19 +28,6 @@ class MainFrame(wx.Frame):
 
     def OnInit(self):
         panel = MainPanel(parent=self)
-        self.SetBackgroundColour('white')
-
-class PicturePointsFrame(wx.Frame):
-    # subclass of wx.Window; Frame is a top level window
-    # A frame is a window whose size and position can (usually) be changed by the user.
-    # Usually represents the first/main window a user will see
-    def __init__(self, title, parent=None):
-        wx.Frame.__init__(self, parent=parent, title=title)
-        self.OnInit()
-        self.Show()
-
-    def OnInit(self):
-        panel = PicturePointsPanel(parent=self)
         self.SetBackgroundColour('white')
 
 class MainPanel(wx.Panel):
@@ -65,27 +53,37 @@ class MainPanel(wx.Panel):
         font12 = wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u'Segoe UI')
         instrText.SetFont(SEGOE_12)
 
-        #get current file directory
-        curdir = os.path.dirname(os.path.realpath(__file__))
 
-        bmp = wx.Image(curdir + "/images/PinButton.PNG", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        print(curdir)
+
+        bmp = wx.Image(curdir + "/images/PinButton.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         pinButton = wx.BitmapButton(self, -1, bmp, pos=(25, 148), size=(400, 60))
         pinButton.SetBackgroundColour(wx.WHITE)
         pinButton.SetWindowStyleFlag(wx.BU_LEFT)
 
-        bmp = wx.Image(curdir + "/images/PictureButton.PNG", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        bmp = wx.Image(curdir + "/images/PictureButton.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         picButton = wx.BitmapButton(self, -1, bmp, pos=(25, 205), size=(400, 60))
         picButton.SetBackgroundColour(wx.WHITE)
         picButton.Bind(wx.EVT_BUTTON, self.openPicturePointsFrame)
-
-    def onSubmit(self, event):
-        # stuff for the submit button to do
-        webbrowser.open('https://wxpython.org/Phoenix/docs/html/index.html')
 
     def openPicturePointsFrame(self, event):
         frame = PicturePointsFrame(title="")
         frame.SetSize(wx.Size(700, 600))
         #self.frame_number += 1
+
+
+class PicturePointsFrame(wx.Frame):
+    # subclass of wx.Window; Frame is a top level window
+    # A frame is a window whose size and position can (usually) be changed by the user.
+    # Usually represents the first/main window a user will see
+    def __init__(self, title, parent=None):
+        wx.Frame.__init__(self, parent=parent, title=title)
+        self.OnInit()
+        self.Show()
+
+    def OnInit(self):
+        panel = PicturePointsPanel(parent=self)
+        self.SetBackgroundColour('white')
 
 class PicturePointsPanel(wx.Panel):
     # A panel is a window on which controls are placed. (e.g. buttons and text boxes)
@@ -103,52 +101,54 @@ class PicturePointsPanel(wx.Panel):
         infoText = wx.StaticText(self, label="Select a picture to use or choose your own:", pos=(20, 65))
         infoText.SetFont(SEGOE_12)
 
-        #current file directory
+        #get current file directory
         curdir = os.path.dirname(os.path.realpath(__file__))
 
         sample1Img = wx.Image(curdir + "/images/sample1.jpg", wx.BITMAP_TYPE_ANY)
         sample1ImgIcon = sample1Img.Scale(192, 108, wx.IMAGE_QUALITY_HIGH)
         sample1ImgIcon = sample1ImgIcon.ConvertToBitmap()
         sample1Button = wx.BitmapButton(self, -1, sample1ImgIcon, pos=(25, 98), size=(192, 108))
-        sample1Button.Bind(wx.EVT_BUTTON, self.selectSampleImage)
+        sample1Button.Bind(wx.EVT_BUTTON, lambda event: self.openPicturePointsSelectFrame(event, sample1Img))
 
         sample2Img = wx.Image(curdir + "/images/sample2.jpg", wx.BITMAP_TYPE_ANY)
         sample2ImgIcon = sample2Img.Scale(192, 108, wx.IMAGE_QUALITY_HIGH)
         sample2ImgIcon = sample2ImgIcon.ConvertToBitmap()
         sample2Button = wx.BitmapButton(self, -1, sample2ImgIcon, pos=(225, 98), size=(192, 108))
-        sample2Button.Bind(wx.EVT_BUTTON, self.selectSampleImage)
+        sample2Button.Bind(wx.EVT_BUTTON, lambda event: self.openPicturePointsSelectFrame(event, sample2Img))
 
         sample3Img = wx.Image(curdir + "/images/sample3.jpg", wx.BITMAP_TYPE_ANY)
         sample3ImgIcon = sample3Img.Scale(192, 108, wx.IMAGE_QUALITY_HIGH)
         sample3ImgIcon = sample3ImgIcon.ConvertToBitmap()
         sample3Button = wx.BitmapButton(self, -1, sample3ImgIcon, pos=(425, 98), size=(192, 108))
-        sample3Button.Bind(wx.EVT_BUTTON, self.selectSampleImage)
+        sample3Button.Bind(wx.EVT_BUTTON, lambda event: self.openPicturePointsSelectFrame(event, sample3Img))
 
         sample4Img = wx.Image(curdir + "/images/sample4.jpg", wx.BITMAP_TYPE_ANY)
         sample4ImgIcon = sample4Img.Scale(192, 108, wx.IMAGE_QUALITY_HIGH)
         sample4ImgIcon = sample4ImgIcon.ConvertToBitmap()
         sample4Button = wx.BitmapButton(self, -1, sample4ImgIcon, pos=(25, 214), size=(192, 108))
-        sample4Button.Bind(wx.EVT_BUTTON, self.selectSampleImage)
+        sample4Button.Bind(wx.EVT_BUTTON, lambda event: self.openPicturePointsSelectFrame(event, sample4Img))
 
         sample5Img = wx.Image(curdir + "/images/sample5.jpg", wx.BITMAP_TYPE_ANY)
         sample5ImgIcon = sample5Img.Scale(192, 108, wx.IMAGE_QUALITY_HIGH)
         sample5ImgIcon = sample5ImgIcon.ConvertToBitmap()
         sample5Button = wx.BitmapButton(self, -1, sample5ImgIcon, pos=(225, 214), size=(192, 108))
-        sample5Button.Bind(wx.EVT_BUTTON, self.selectSampleImage)
+        sample5Button.Bind(wx.EVT_BUTTON, lambda event: self.openPicturePointsSelectFrame(event, sample5Img))
 
         sample6Img = wx.Image(curdir + "/images/sample6.jpg", wx.BITMAP_TYPE_ANY)
         sample6ImgIcon = sample6Img.Scale(192, 108, wx.IMAGE_QUALITY_HIGH)
         sample6ImgIcon = sample6ImgIcon.ConvertToBitmap()
         sample6Button = wx.BitmapButton(self, -1, sample6ImgIcon, pos=(425, 214), size=(192, 108))
-        sample6Button.Bind(wx.EVT_BUTTON, self.selectSampleImage)
+        sample6Button.Bind(wx.EVT_BUTTON, lambda event: self.openPicturePointsSelectFrame(event, sample6Img))
 
         chooseImgButton = wx.Button(self, label="Choose your own", pos=(25, 345), size=(145, 35))
         chooseImgButton.SetBackgroundColour(wx.WHITE)
         chooseImgButton.SetFont(SEGOE_12)
         chooseImgButton.Bind(wx.EVT_BUTTON, self.onOpenFile)
 
-    def selectSampleImage(self, event):
-        print("Selected ")
+    def openPicturePointsSelectFrame(self, event, img):
+        frame = PicturePointsSelectFrame(img=img, title="")
+        frame.SetSize(wx.Size(img.Width, img.Height))
+        wx.MessageBox("To set your password, click a series of 4 points on the image", " ", wx.OK | wx.ICON_INFORMATION)
 
     def onOpenFile(self, event):
         wildcard = "Image files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png|" \
@@ -167,6 +167,55 @@ class PicturePointsPanel(wx.Panel):
             for path in paths:
                 print(path)
         dlg.Destroy()
+
+
+class PicturePointsSelectFrame(wx.Frame):
+    # subclass of wx.Window; Frame is a top level window
+    # A frame is a window whose size and position can (usually) be changed by the user.
+    # Usually represents the first/main window a user will see
+    def __init__(self, img, title, parent=None):
+        wx.Frame.__init__(self, parent=parent, title=title, pos=(0, 0))
+        self.OnInit(img)
+        self.Show()
+
+    def OnInit(self, img):
+        panel = PicturePointsSelectPanel(parent=self, img=img)
+        self.SetBackgroundColour('white')
+
+class PicturePointsSelectPanel(wx.Panel):
+    selection = []
+    # A panel is a window on which controls are placed. (e.g. buttons and text boxes)
+    # wx.Panel class is usually put inside a wxFrame object. This class is also inherited from wxWindow class.
+    def __init__(self, parent, img):
+        super().__init__(parent=parent)
+        self.img = img
+        self.bmpImg = self.img.ConvertToBitmap()
+        self.sbmpImg = wx.StaticBitmap(self, -1, self.bmpImg, (1, 1), (self.img.GetWidth(), self.img.GetHeight()))
+        self.sbmpImg.Bind(wx.EVT_LEFT_DOWN, self.onClick)
+        self.selection = []
+        self.Show(True)
+
+    def onClick(self, event):
+        pos = event.GetPosition()
+        self.selection.append(pos)
+        dc = wx.MemoryDC(self.bmpImg)
+        dc.SetBrush(wx.Brush(wx.GREEN))
+        dc.DrawCircle(pos.x, pos.y, 7)
+        dc.SelectObject(wx.NullBitmap)
+        self.sbmpImg.SetBitmap(self.bmpImg)
+        if (len(self.selection) == 4):
+            happySelection = wx.MessageDialog(None, "Are you happy with your selection?", " ", wx.YES | wx.NO | wx.ICON_INFORMATION)
+            if happySelection.ShowModal() == wx.ID_NO:
+                self.selection = []
+                #dc.Clear()
+                #self.Show(True)
+                #dc.SelectObject(self.img)
+                self.bmpImg = self.img.ConvertToBitmap()
+                self.sbmpImg.SetBitmap(self.bmpImg)
+            else:
+                print("Save selection")
+                self.GetParent().Close()
+                wx.MessageBox("Password has been saved", " ", wx.OK | wx.ICON_INFORMATION)
 
 if __name__ == "__main__":
     app = MyApp()

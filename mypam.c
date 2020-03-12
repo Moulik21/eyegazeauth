7bee80464c519c31440f92e42c053a9b29ec04c4
Daniel Bishara <danielb3@live.ca>
2020-03-11 19:22:05 -0500

    Update mypam.c
    Added support for wx python, cleaned up code

--- mypam.c
+++ mypam.c
@@ -5,55 +5,63 @@
 #include <security/pam_modules.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
-/* expected hook */
+#include <errno.h>
+ 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
 	return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("Acct mgmt\n");
 	return PAM_SUCCESS;
 }
 
-/* expected hook, this is where custom stuff happens */
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
 	int retval;
 
 	const char* pUsername;
-	retval = pam_get_user(pamh, &pUsername, "Username: ");
-	
+
 	int waitstatus;
 	pid_t pid;
 	int ret = 1;
 	
-	struct sigaction newact, oldact;
-	newact.sa_handler = SIG_DFL;
-	newact.sa_flags = 0;
-	sigfillset(&newact.sa_mask);
-	sigaction (SIGCHLD, &newact, &oldact);
-	
 	pid = fork();
-	char *parms[] = {"/usr/bin/python", "/lib/security/src/button.py", NULL};   
+	char *parms[] = {"python", "/lib/security/button3.py", NULL};   
+	
 	if(pid < 0) {
         	fprintf(stderr, "Fork failed");
         	return 1;
 	}
+
 	else if(pid == 0) {
-        	ret = execv(parms[0], parms);
+
+	char uid[1000];
+	char gid[1000];
+    	FILE *fptr;
+    	if ((fptr = fopen("/lib/security/pamuser.txt", "r")) == NULL) {
+        	printf("Error! Cannot find PamUser Account!");
+       		exit(1);
+   	 }
+
+   
+   	fscanf(fptr, "%[^\n]", uid);
+   	fscanf(fptr, "%[^\n]", gid);
+   
+    	fclose(fptr);
+ 	setuid(atoi(uid));
+  	setgid(atoi(gid));
+  
+      	ret = execvp(parms[0], parms);
+	//We should never get here!
+	exit(1);
 	}
 	else {
-        	wait(&waitstatus);
-		sigaction (SIGCHLD, &oldact, NULL);
-        	ret = WEXITSTATUS(waitstatus);
+		wait(&waitstatus);
+		ret = WEXITSTATUS(waitstatus);
 	}
 
 	if (ret != 0){
 		return PAM_ABORT;
 	}
 
-	printf("Welcome %s\n", pUsername);
-	
-	
 	return PAM_SUCCESS;
 }
